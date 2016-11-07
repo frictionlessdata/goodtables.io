56d2aeba9c3e4e5fde27680650f5f85c30f3a620
amercader <amercadero@gmail.com>
2016-11-07 14:09:48 +0000

    Add basic boilerplate

---
+++ setup.py
@@ -0,0 +1,63 @@
+import os
+import io
+from setuptools import setup, find_packages
+
+
+# Helpers
+def read(*paths):
+    """Read a text file."""
+    basedir = os.path.dirname(__file__)
+    fullpath = os.path.join(basedir, *paths)
+    contents = io.open(fullpath, encoding='utf-8').read().strip()
+    return contents
+
+
+# Prepare
+PACKAGE = 'goodtablesio'
+NAME = PACKAGE.replace('_', '-')
+DESCRIPTION = '''
+An online continuous data validation service
+'''
+INSTALL_REQUIRES = [
+]
+TESTS_REQUIRE = [
+    'pylama',
+    'tox',
+]
+README = read('README.md')
+VERSION = read(PACKAGE, 'VERSION')
+PACKAGES = find_packages(exclude=['examples', 'tests'])
+
+
+# Run
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=PACKAGES,
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'develop': TESTS_REQUIRE},
+    zip_safe=False,
+    long_description=README,
+    description=DESCRIPTION,
+    author='Open Knowledge International',
+    author_email='info@okfn.org',
+    url='https://github.com/frictionlessdata/goodtables.io',
+    license='GNU Affero General Public License (AGPL) v3.0',
+    keywords=[
+        'data', 'validation', 'csv', 'excel'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
