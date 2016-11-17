fc8eed8a2ff3b4154548c49d9fa5a8ac1c0d3392
roll <roll@post.agency>
2016-11-17 13:21:06 +0300

    renamed api response report to result
    There is validation report already.

--- goodtablesio/helpers/retrieve.py
+++ goodtablesio/helpers/retrieve.py
@@ -13,11 +13,11 @@ def get_job(job_id):
         dict: job result
 
     """
-    report = services.database['jobs'].find_one(job_id=job_id)
+    result = services.database['jobs'].find_one(job_id=job_id)
     # TODO: we need to store the status in the DB as we can no longer rely on
     # the job id being the same one used by a celery task
     status = 'Not Implemented'
-    return {'status': status, 'report': report}
+    return {'status': status, 'result': result}
 
 
 def get_job_ids():
