4d64841bcf9a4eb4862643f9038d865c0b9dacf5
amercader <amercadero@gmail.com>
2016-11-28 11:06:35 +0000

    Make sure report is part of the job dict
    To get around failures like this for now:
    
    https://travis-ci.org/frictionlessdata/goodtables.io/builds/179121528
    
    This should definitely be investigated and fixed as part of #33

--- goodtablesio/helpers/retrieve.py
+++ goodtablesio/helpers/retrieve.py
@@ -20,6 +20,13 @@ def get_job(job_id):
     # TODO: we need to store the status in the DB as we can no longer rely on
     # the job id being the same one used by a celery task
     status = 'Not Implemented'
+
+    # TODO: this should not be needed after #33
+    if 'report' not in result:
+        result['report'] = None
+    if 'finished' not in result:
+        result['finished'] = None
+
     return {'status': status, 'result': result}
 
 
