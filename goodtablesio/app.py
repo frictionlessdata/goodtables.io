6abaf81b8caa3e3fadcb4f44253784cd51af0d2c
roll <roll@post.agency>
2016-11-29 11:45:30 +0300

    improved UI

--- goodtablesio/app.py
+++ goodtablesio/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from .blueprints.api import api
 from .blueprints.site import site
 from .plugins.github.blueprint import github
@@ -17,3 +17,15 @@ app.register_blueprint(site)
 
 # Register plugins
 app.register_blueprint(github)
+
+
+# Set error handlers
+
+@app.errorhandler(404)
+def not_found_error(err):
+    return (render_template('error404.html'), 404)
+
+
+@app.errorhandler(500)
+def server_error(err):
+    return (render_template('error500.html'), 500)
