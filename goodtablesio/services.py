93905c42a472893823cec795a8e7645b9e11c334
amercader <amercadero@gmail.com>
2016-12-01 12:25:55 +0000

    Hack to drop the table on cluster

--- goodtablesio/services.py
+++ goodtablesio/services.py
@@ -7,3 +7,6 @@ from . import config
 
 database = dataset.connect(config.DATABASE_URL)
 database.get_table('jobs', primary_id='job_id', primary_type='String')
+
+database['jobs'].drop()
+database.get_table('jobs', primary_id='job_id', primary_type='String')
