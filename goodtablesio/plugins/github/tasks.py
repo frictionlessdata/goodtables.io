9232a17499ec44f2b6fc97b1d7b484f0e059b6c6
roll <eskarev@gmail.com>
2016-12-07 14:10:37 +0300

    rebased github clone to github api

--- goodtablesio/plugins/github/tasks.py
+++ goodtablesio/plugins/github/tasks.py
@@ -1,81 +1,60 @@
-import re
-import os
-import subprocess
-import tempfile
-import logging
-import shutil
+from functools import partial
+from github3 import GitHub
 
-from goodtablesio import config, helpers
+from goodtablesio import helpers
 from goodtablesio.tasks import app as celery_app, JobTask
-
-log = logging.getLogger(__name__)
 
 
 # Module API
 
 @celery_app.task(name='goodtablesio.github.get_validation_conf', base=JobTask)
-def get_validation_conf(clone_url, job_id):
-    # We need to import the DB connection at this point, as it has been
-    # initialized when the worker started
-    from goodtablesio.tasks import tasks_db
+def get_validation_conf(owner, repo, job_id):
 
+    # Update job in database
+    from goodtablesio.tasks import tasks_db
     tasks_db['jobs'].update({'job_id': job_id, 'status': 'running'}, ['job_id'])
 
-    clone_dir = _clone_repo(job_id, clone_url)
-    job_conf_url = _get_job_conf_url(clone_url)
-    job_files = _get_job_files(clone_dir)
-
-    # Get job descriptor
-    validation_conf = helpers.prepare_job(job_conf_url, job_files)
-
-    # TODO: handle exceptions (eg bad task description)
-
-    _remove_repo(clone_url, clone_dir)
+    # Get validation conf
+    job_conf_url, job_file_urls = _get_job_urls(owner, repo)
+    validation_conf = helpers.prepare_job(job_conf_url, job_file_urls)
 
     return validation_conf
 
 
 # Internal
 
-def _clone_repo(job_id, clone_url):
-    clone_dir = tempfile.mkdtemp(prefix=job_id, dir=config.GITHUB_CLONE_DIR)
-    clone_command = ['git', 'clone', clone_url, clone_dir]
-    log.info('Cloning repo {0} into {1}'.format(clone_url, clone_dir))
-    try:
-        subprocess.check_output(clone_command, stderr=subprocess.STDOUT)
-        log.debug('Repo {0} cloned'.format(clone_url))
-    except subprocess.CalledProcessError as e:
-        log.error(e.output)
-        raise e
-    return clone_dir
+def _get_job_urls(owner, repo):
+    make_url = partial(_make_url, owner=owner, repo=repo)
+    repo_api = GitHub().repository(owner, repo)
+    contents = repo_api.contents('/')
+    conf_url = _get_job_conf_url(contents, make_url)
+    file_urls = _get_job_file_urls(contents, repo_api, make_url)
+    return (conf_url, file_urls)
 
 
-def _get_job_conf_url(clone_url, branch='master'):
-    pattern = r'github.com/(?P<user>[^/]*)/(?P<repo>[^/]*)\.git'
-    match = re.search(pattern, clone_url)
-    user = match.group('user')
-    repo = match.group('repo')
-    template = '{base}/{user}/{repo}/{branch}/goodtables.yml'
-    job_conf = template.format(
+def _get_job_conf_url(contents, make_url):
+    key = 'goodtables.yml'
+    if key not in contents:
+        return None
+    url = make_url(contents[key].path)
+    return url
+
+
+def _get_job_file_urls(contents, repo_api, make_url):
+    urls = []
+    for key in sorted(contents):
+        item = contents[key]
+        if item.type == 'file':
+            urls.append(make_url(item.path))
+        elif item.type == 'dir':
+            dir_contents = repo_api.contents(item.path)
+            urls.extend(_get_job_file_urls(dir_contents, repo_api, make_url))
+    return urls
+
+
+def _make_url(path, owner, repo, branch='master'):
+    template = '{base}/{owner}/{repo}/{branch}/{path}'
+    url = template.format(
         base='https://raw.githubusercontent.com',
-        user=user, repo=repo, branch=branch)
-    return job_conf
-
-
-def _get_job_files(top):
-    out = []
-    for dir_name, sub_dir_list, file_list in os.walk(top, topdown=True):
-        if '.git' in sub_dir_list:
-            sub_dir_list.remove('.git')
-        current_dir = dir_name.replace(top, '').lstrip('/')
-        for file_name in file_list:
-            if current_dir:
-                out.append(os.path.join(current_dir, file_name))
-            else:
-                out.append(file_name)
-    return out
-
-
-def _remove_repo(clone_url, clone_dir):
-    log.debug('Removing repo {0}'.format(clone_url))
-    shutil.rmtree(clone_dir)
+        owner=owner, repo=repo, branch=branch, path=path)
+    return url
--- goodtablesio/tests/plugins/github/test_tasks.py
+++ goodtablesio/tests/plugins/github/test_tasks.py
@@ -1,59 +1,16 @@
-from unittest import mock
-
+# pylama:skip=1
 from goodtablesio.plugins.github import tasks
 
 
-def test_get_job_conf_url():
+# Tests
 
-    clone_url = 'https://github.com/my-org/my-repo.git'
-
-    expected_url = \
-        'https://raw.githubusercontent.com/my-org/my-repo/master/goodtables.yml'
-    assert tasks._get_job_conf_url(clone_url) == expected_url
-
-
-def test_get_job_conf_url_custom_branch():
-
-    clone_url = 'https://github.com/my-org/my-repo.git'
-
-    expected_url = \
-        'https://raw.githubusercontent.com/my-org/my-repo/fix/goodtables.yml'
-    assert tasks._get_job_conf_url(clone_url, branch='fix') == expected_url
-
-
-def test_get_job_files():
-
-    with mock.patch('os.walk') as mock_walk:
-        '''
-        This mocks the following file structure:
-
-            /my/folder/.git/file0.txt
-            /my/folder/folder1/ (empty)
-            /my/folder/folder2/file1.csv
-            /my/folder/folder2/file2.pdf
-            /my/folder/folder3/folder4/file3.pdf
-            /my/folder/folder3/folder4/file4.csv
-            /my/folder/folder3/folder4/file5.csv
-            /my/folder/file6.csv
-            /my/folder/file7.txt
-        '''
-
-        mock_walk.return_value = [
-            ('/my/folder', ['.git', 'folder1', 'folder2', 'folder3'],
-                ['file6.csv', 'file7.txt']),
-            ('/my/folder/folder1', [], []),
-            ('/my/folder/folder2', [], ['file1.csv', 'file2.pdf']),
-            ('/my/folder/folder3', ['folder4'], []),
-            ('/my/folder/folder3/folder4', [],
-                ['file3.pdf', 'file4.csv', 'file5.csv']),
+def test_get_job_urls():
+    assert tasks._get_job_urls('frictionlessdata', 'goodtables.io-example') == (
+        'https://raw.githubusercontent.com/frictionlessdata/goodtables.io-example/master/goodtables.yml',
+        [
+            'https://raw.githubusercontent.com/frictionlessdata/goodtables.io-example/master/README.md',
+            'https://raw.githubusercontent.com/frictionlessdata/goodtables.io-example/master/data/invalid.csv',
+            'https://raw.githubusercontent.com/frictionlessdata/goodtables.io-example/master/goodtables.yml',
+            'https://raw.githubusercontent.com/frictionlessdata/goodtables.io-example/master/valid.csv',
         ]
-
-        assert tasks._get_job_files('/my/folder') == [
-            'file6.csv',
-            'file7.txt',
-            'folder2/file1.csv',
-            'folder2/file2.pdf',
-            'folder3/folder4/file3.pdf',
-            'folder3/folder4/file4.csv',
-            'folder3/folder4/file5.csv',
-        ]
+    )
