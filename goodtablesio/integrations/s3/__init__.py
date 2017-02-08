260b70b8f064717009cc6c97ba981ec0fdab859c
amercader <amercadero@gmail.com>
2017-02-08 14:53:12 +0000

    Use a signal to clean up the tables source URL on reports

--- goodtablesio/integrations/s3/__init__.py
+++ goodtablesio/integrations/s3/__init__.py
@@ -3,3 +3,6 @@ import goodtablesio.integrations.s3.models.bucket
 
 # Register tasks
 import goodtablesio.integrations.s3.tasks.jobconf
+
+# Register signals
+import goodtablesio.integrations.s3.signals
