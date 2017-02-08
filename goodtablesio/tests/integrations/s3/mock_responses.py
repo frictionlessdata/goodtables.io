6fd507ad1d52460ddaec82c1285f3816e075613c
amercader <amercadero@gmail.com>
2017-02-08 10:44:31 +0000

    Helper to parse the hook payload (an S3 event)

--- goodtablesio/tests/integrations/s3/mock_responses.py
+++ goodtablesio/tests/integrations/s3/mock_responses.py
@@ -149,3 +149,43 @@ lambda_add_permission = {
 lambda_remove_permission = {
     'ResponseMetadata': response_metadata(204)
 }
+
+
+s3_notification_event = {
+  'Records': [
+    {
+      'eventVersion': '2.0',
+      'eventTime': '1970-01-01T00:00:00.000Z',
+      'requestParameters': {
+        'sourceIPAddress': '127.0.0.1'
+      },
+      's3': {
+        'configurationId': 'testConfigRule',
+        'object': {
+          'eTag': '0123456789abcdef0123456789abcdef',
+          'key': 'councillors-address.csv',
+          'sequencer': '0A1B2C3D4E5F678901',
+          'size': 1024
+        },
+        'bucket': {
+          'ownerIdentity': {
+            'principalId': 'EXAMPLE'
+          },
+          'name': 'test-gtio-1',
+          'arn': 'arn:aws:s3:::test-gtio-1'
+        },
+        's3SchemaVersion': '1.0'
+      },
+      'responseElements': {
+        'x-amz-id-2': 'EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH',
+        'x-amz-request-id': 'EXAMPLE123456789'
+      },
+      'awsRegion': 'us-east-1',
+      'eventName': 'ObjectCreated:Put',
+      'userIdentity': {
+        'principalId': 'EXAMPLE'
+      },
+      'eventSource': 'aws:s3'
+    }
+  ]
+}
