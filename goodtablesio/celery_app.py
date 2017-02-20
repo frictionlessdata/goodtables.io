1c0776d026292d8246d9215844acb228aa0007aa
Paul Walsh <paulywalsh@gmail.com>
2017-02-20 17:22:02 +0200

    Add Sentry to Celery

--- goodtablesio/celery_app.py
+++ goodtablesio/celery_app.py
@@ -1,8 +1,17 @@
-from celery import Celery
+import celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
+
 from goodtablesio import settings
 
 
-# Module API
+class Celery(celery.Celery):
 
-celery_app = Celery('tasks')
-celery_app.config_from_object(settings)
+    def on_configure(self):
+        client = raven.Client(settings.SENTRY_DSN)
+        register_logger_signal(client)  # defaults to logging.ERROR
+        register_signal(client)
+
+
+app = Celery(__name__)
+app.config_from_object(settings)
