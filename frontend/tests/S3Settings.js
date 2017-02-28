0c1c6dacbcd08333ba83b5bb81628dd4d58577e3
roll <eskarev@gmail.com>
2017-02-28 21:45:07 +0300

    recovered frontend tests

--- frontend/tests/S3Settings.js
+++ frontend/tests/S3Settings.js
@@ -1,40 +1,60 @@
+import axios from 'axios'
 import {should} from 'chai'
 import {mount} from 'avoriaz'
+import AxiosMockAdapter from 'axios-mock-adapter'
+import Messages from '../components/Messages.vue'
 import S3Settings from '../components/S3Settings.vue'
 should()
 
 // Tests
 
 describe('S3Settings', () => {
+  let mockAxios
 
-  it('should contain headings', () => {
-    const propsData = {}
-    const wrapper = mount(S3Settings, {propsData})
-    wrapper.find('h1')[0].text().should.equal('Amazon S3')
-    wrapper.find('h2')[0].text().should.equal('Buckets')
-    wrapper.find('h2')[1].text().should.equal('Add Bucket')
-  })
-
-  it('should have no buckets', () => {
-    const propsData = {buckets: []}
-    const wrapper = mount(S3Settings, {propsData})
-    wrapper.text().should.include('No buckets configured')
-  })
-
-  describe('[with buckets]', () => {
-
-    it('should contain buckets', () => {
-      const propsData = {
-        buckets: [
-          {name: 'name1'},
-          {name: 'name2'},
-        ],
-      }
-      const wrapper = mount(S3Settings, {propsData})
-      wrapper.find('[href="/s3/settings/remove_bucket/name1"]').should.has.length(1)
-      wrapper.find('[href="/s3/settings/remove_bucket/name2"]').should.has.length(1)
+  beforeEach(() => {
+    mockAxios = new AxiosMockAdapter(axios)
+    mockAxios.onGet('/s3/api/bucket').reply(200, {
+      buckets: [],
     })
+  })
 
+  afterEach(() => {
+    mockAxios.restore()
+  })
+
+  it('should contain headings', (done) => {
+    const wrapper = mount(S3Settings)
+    setTimeout(() => {
+      wrapper.find('h1')[0].text().should.equal('Amazon S3')
+      wrapper.find('h2')[0].text().should.equal('Buckets')
+      wrapper.find('h2')[1].text().should.equal('Add Bucket')
+      done()
+    })
+  })
+
+  it('should have no buckets', (done) => {
+    const wrapper = mount(S3Settings)
+    setTimeout(() => {
+      wrapper.text().should.include('No buckets configured')
+      done()
+    })
+  })
+
+  it('should show buckets after loading', (done) => {
+    mockAxios.reset()
+    mockAxios.onGet('/s3/api/bucket').reply(200, {
+      buckets: [
+        {id: 'id1', name: 'name1', active: true},
+        {id: 'id2', name: 'name2', active: false},
+      ],
+    })
+    const wrapper = mount(S3Settings)
+    wrapper.text().should.include('Loading buckets. Please wait..')
+    setTimeout(() => {
+      wrapper.text().should.include('name1')
+      wrapper.text().should.include('name2')
+      done()
+    })
   })
 
 })
