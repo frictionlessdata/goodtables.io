7b131bd69ea7be4b17bbd6782b31c3f181694f21
roll <eskarev@gmail.com>
2017-04-19 17:52:37 +0300

    updated Source component to be a page

--- frontend/components/Source.vue
+++ frontend/components/Source.vue
@@ -6,6 +6,7 @@ export default {
   name: 'Source_',
   props: {
     source: Object,
+    job: Object,
   },
   components: {
     Report,
@@ -14,10 +15,10 @@ export default {
   computed: {
     statusClass() {
       return {
-        valid: this.source.last_job && this.source.last_job.status === 'success',
-        invalid: this.source.last_job && this.source.last_job.status === 'failure',
-        error: this.source.last_job && this.source.last_job.status === 'error',
-        info: !this.source.last_job,
+        valid: this.job && this.job.status === 'success',
+        invalid: this.job && this.job.status === 'failure',
+        error: this.job && this.job.status === 'error',
+        info: !this.job,
       }
     },
   },
@@ -26,52 +27,61 @@ export default {
 </script>
 
 <template>
-  <div v-bind:class="statusClass">
-    <div class="inner banner">
-      <template v-if="source.integration_name === 'github'">
-      <a class="icon-github integration"><i>GitHub</i></a>
-      </template>
+  <div class="app">
+    <div class="inner">
+      <div class="default">
 
-      <template v-else-if="source.integration_name === 's3'">
-      <a class="icon-amazon integration"><i>Amazon S3</i></a>
-      </template>
-      <h2 class="source-title">
-          {{ source.name }}
-      </h2>
+        <main class="source-view">
+          <div v-bind:class="statusClass">
+            <div class="inner banner">
+              <template v-if="source.integration_name === 'github'">
+              <a class="icon-github integration"><i>GitHub</i></a>
+              </template>
 
-      <SourceListItem :source="source" :inSourcePanel="true"/>
-    </div>
-    <section class="inner">
+              <template v-else-if="source.integration_name === 's3'">
+              <a class="icon-amazon integration"><i>Amazon S3</i></a>
+              </template>
+              <h2 class="source-title">
+                  {{ source.name }}
+              </h2>
 
-      <div>
-        <ul class="nav nav-tabs" role="tablist">
-         <li role="presentation" class="active"><a href="#report" aria-controls="home" role="tab" data-toggle="tab">Report</a></li>
-         <li role="presentation"><a href="#history" aria-controls="profile" role="tab" data-toggle="tab">Job history</a></li>
-        </ul>
-        <div class="tab-content">
-          <div role="tabpanel" class="report tab-pane active" id="report">
+              <SourceListItem :source="source" :inSourcePanel="true"/>
+            </div>
+            <section class="inner">
 
-            <template v-if="source.last_job">
-            <ul class="meta">
-                Report calculated on {{ source.last_job.finished }}
-              </li>
-              <li>
-                Source added: 21 Feb 2017
-              </li>
-            </ul>
-            <Report :report="source.last_job.report" />
-            </template>
+              <div>
+                <ul class="nav nav-tabs" role="tablist">
+                 <li role="presentation" class="active"><a href="#report" aria-controls="home" role="tab" data-toggle="tab">Report</a></li>
+                 <li role="presentation"><a href="#history" aria-controls="profile" role="tab" data-toggle="tab">Job history</a></li>
+                </ul>
+                <div class="tab-content">
+                  <div role="tabpanel" class="report tab-pane active" id="report">
 
-            <template v-if="!source.last_job">
-            <p>No jobs yet</p>
-            </template>
-         </div>
-         <div role="tabpanel" class="tab-pane" id="history">
-           History here
-         </div>
-        </div>
+                    <template v-if="job">
+                    <ul class="meta">
+                        Report calculated on {{ job.finished }}
+                      </li>
+                      <li>
+                        Source added: 21 Feb 2017
+                      </li>
+                    </ul>
+                    <Report :report="job.report" />
+                    </template>
+
+                    <template v-if="!job">
+                    <p>No jobs yet</p>
+                    </template>
+                 </div>
+                 <div role="tabpanel" class="tab-pane" id="history">
+                   History here
+                 </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+
       </div>
-    </section>
+    </div>
   </div>
 </template>
-
