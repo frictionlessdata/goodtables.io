52a5d17875b2c8e29e1c187b0b53db32834d1f4b
Sam Smith <sam@mintcanary.com>
2017-05-02 11:15:35 +0100

    style footer

--- frontend/components/Home.vue
+++ frontend/components/Home.vue
@@ -118,7 +118,21 @@ export default {
       </div>
     </section>
     <section class="footer">
-		<span>&copy; <a href="https://okfn.org">Open Knowledge International</a> 2017</span> | <span><a href="https://twitter.com/OKFNLabs">@OKFNLabs</a></span>
+      <div class="container">
+        <div class="footer-primary">
+          <a class="footer-logo" href="https://okfn.org/">
+            <img src="https://a.okfn.org/img/oki/landscape-white-468x122.png" alt="Open Knowledge International">
+          </a>
+          <ul class="footer-links">
+            <li><a href="https://twitter.com/OKFNLabs">@OKFNLabs</a></li>
+          </ul>
+        </div>
+        <div class="footer-secondary">
+          <p>
+            &copy; <a href="https://okfn.org">Open Knowledge International</a> 2017
+          </p>
+        </div>
+      </div>
     </section>
   </div>
 </template>
