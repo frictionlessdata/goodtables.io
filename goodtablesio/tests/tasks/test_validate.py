ee37eb172f2ecfda8c3d1ba111cc270c8ddfae73
amercader <amercadero@gmail.com>
2017-05-10 16:16:06 +0100

    [#206] Increase limit of tables per source
    goodtables has a (rather low) default of 10 tables for inspector. Bump
    that to 100

--- goodtablesio/tests/tasks/test_validate.py
+++ goodtablesio/tests/tasks/test_validate.py
@@ -1,7 +1,7 @@
 import pytest
 import datetime
 from unittest import mock
-from goodtablesio import models
+from goodtablesio import models, settings
 from goodtablesio.tasks.validate import validate
 from goodtablesio.tests import factories
 pytestmark = pytest.mark.usefixtures('session_cleanup')
@@ -48,3 +48,70 @@ def test_validate_skip_rows():
     conf = {'source': [{'source': source, 'format': format, 'skip_rows': ['#']}]}
     job = validate(conf, job_id=job.id)
     assert job['report']['valid'] is True
+
+
+def test_validate_more_than_10_tables():
+
+    # We need to save it on the DB so the session used by the tasks can find it
+    job = factories.Job()
+
+    validation_conf = {
+            'source': [
+                {'source': 'http://example.com/file{}'.format(i)} for i in range(12)
+            ],
+            'settings': {}
+    }
+    validate(validation_conf, job_id=job.id)
+
+    jobs = models.job.find()
+    assert len(jobs[0]['report']['tables']) == 12
+
+
+def test_validate_more_than_10_tables_no_settings():
+
+    # We need to save it on the DB so the session used by the tasks can find it
+    job = factories.Job()
+
+    validation_conf = {
+            'source': [
+                {'source': 'http://example.com/file{}'.format(i)} for i in range(12)
+            ],
+    }
+    validate(validation_conf, job_id=job.id)
+
+    jobs = models.job.find()
+    assert len(jobs[0]['report']['tables']) == 12
+
+
+def test_validate_more_than_100_tables():
+
+    # We need to save it on the DB so the session used by the tasks can find it
+    job = factories.Job()
+
+    validation_conf = {
+            'source': [
+                {'source': 'http://example.com/file{}'.format(i)} for i in range(110)
+            ],
+            'settings': {}
+    }
+    validate(validation_conf, job_id=job.id)
+
+    jobs = models.job.find()
+    assert len(jobs[0]['report']['tables']) == settings.MAX_TABLES_PER_SOURCE
+
+
+def test_validate_more_than_100_tables_even_if_settings():
+
+    # We need to save it on the DB so the session used by the tasks can find it
+    job = factories.Job()
+
+    validation_conf = {
+            'source': [
+                {'source': 'http://example.com/file{}'.format(i)} for i in range(110)
+            ],
+            'settings': {'table_limit': 200}
+    }
+    validate(validation_conf, job_id=job.id)
+
+    jobs = models.job.find()
+    assert len(jobs[0]['report']['tables']) == settings.MAX_TABLES_PER_SOURCE
