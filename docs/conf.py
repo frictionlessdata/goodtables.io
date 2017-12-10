8b5b3d5e7b21e43b422ff5989039bda641c53df1
Vitor Baptista <vitor@vitorbaptista.com>
2017-12-10 22:17:24 +0000

    [#213] Add intial API documentation
    The docs were generated using the OpenAPI specification. Ideally, the
    specification file would be generated directly from our API definition, so
    they're always in sync. We don't do it now, but will in the future.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,10 @@ from recommonmark.transform import AutoStructify
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.openapi',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
