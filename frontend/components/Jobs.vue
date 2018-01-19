f758c9d29e034094c3a7f7157c0edf807462fe7b
roll <eskarev@gmail.com>
2018-01-19 13:48:13 +0300

    Implemented job filter

--- frontend/components/Jobs.vue
+++ frontend/components/Jobs.vue
@@ -6,6 +6,11 @@ export default {
   props: {
     sources: Array,
   },
+  data() {
+    return {
+      filter: '',
+    }
+  },
   components: {
     Job,
   },
@@ -13,8 +18,15 @@ export default {
     lastJobs() {
       return this.sources
         .filter(source => source.last_job)
-        .map(source => source.last_job)
+        .map(source => ({...source.last_job, source}))
     },
+    filteredLastJobs() {
+      if (!this.filter) return this.lastJobs
+      return this.lastJobs.filter(job => {
+        const sourceName = `${job.source.integration_name}/${job.source.name}`
+        return sourceName.includes(this.filter)
+      })
+    }
   }
 }
 </script>
@@ -23,18 +35,29 @@ export default {
   <div class="only jobs">
     <h1>Jobs</h1>
 
+    <!-- Filter -->
     <div class="filter form-group">
       <label class="sr-only" for="keyword">Filter by keyword</label>
       <div class="input-group">
-        <input type="text" class="form-control input-lg" id="keyword" placeholder="Filter by keyword">
+        <input
+          v-model="filter"
+          type="text"
+          class="form-control input-lg"
+          id="keyword"
+          placeholder="Filter by keyword"
+        >
         <div class="input-group-addon">
           <button><span class="icon-search"><i>Search</i></span></button>
         </div>
       </div>
     </div>
 
-    <div class="source-list">
-      <Job view="standard" :job="job" v-for="job of lastJobs" />
+    <!-- Jobs -->
+    <div v-if="filteredLastJobs" class="source-list">
+      <Job view="standard" :job="job" v-for="job of filteredLastJobs" />
+    </div>
+    <div v-else class="empty">
+      <p>No jobs found.</p>
     </div>
 
   </div>
