e9b360af982b62c65219c140f476a57a47fe0ab8
Stephen Abbott Pugh <stephendabbott@gmail.com>
2019-05-17 10:58:24 +0200

    Change OKI to OKF

--- frontend/components/App.vue
+++ frontend/components/App.vue
@@ -91,7 +91,7 @@ export default {
               <div>
                 <p>To provide feedback or report an issue:</p>
                 <ul>
-                  <li>Give feedback <a href="https://discuss.okfn.org/t/launching-goodtables-io-tell-us-what-you-think" target="_blank">thread</a> on the Open Knowledge International forum.</li>
+                  <li>Give feedback <a href="https://discuss.okfn.org/t/launching-goodtables-io-tell-us-what-you-think" target="_blank">thread</a> on the Open Knowledge Foundation forum.</li>
                   <li>Open a bug report or a feature request on the <a href="https://github.com/frictionlessdata/goodtables.io/issues" target="_blank">goodtables.io issue tracker</a>.</li>
                   <li>Join our <a href="https://gitter.im/frictionlessdata/chat" target="_blank">chat room on Gitter, and talk to the team</a>.</li>
                 </ul>
